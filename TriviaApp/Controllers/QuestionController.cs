c8b376ceefedafc4523c28193988a00b81f0fd15
Barrett Helzel <barrett.p.helzel@gmail.com>
2020-04-23 12:58:14 -0700

    * .gitignore: Add question and answer routes and controller methods
    * TriviaApp.sln:
    * Program.cs:
    * Startup.cs:
    * TriviaApp.csproj:
    * appsettings.json:
    * site.js:
    * favicon.ico:
    * site.css:
    * Index.cshtml:
    * _ViewStart.cshtml:
    * ErrorViewModel.cs:
    * Privacy.cshtml:
    * Error.cshtml:
    * _ViewImports.cshtml:
    * _Layout.cshtml:
    * appsettings.Development.json:
    * HomeController.cs:
    * LICENSE:
    * LICENSE.txt:
    * launchSettings.json:
    * QuestionController.cs:
    * jquery.js:
    * jquery.min.js:
    * jquery.min.map:
    * LICENSE.md:
    * bootstrap.js:
    * bootstrap.css:
    * _ValidationScriptsPartial.cshtml:
    * bootstrap.min.js:
    * bootstrap.js.map:
    * bootstrap.css.map:
    * bootstrap.min.css:
    * bootstrap-grid.css:
    * bootstrap.bundle.js:
    * bootstrap.min.js.map:
    * bootstrap-reboot.css:
    * bootstrap.min.css.map:
    * LICENSE.txt:
    * bootstrap.bundle.js.map:
    * bootstrap.bundle.min.js:
    * jquery.validate.js:
    * bootstrap-grid.css.map:
    * bootstrap-grid.min.css:
    * bootstrap-reboot.css.map:
    * bootstrap-reboot.min.css:
    * additional-methods.js:
    * bootstrap.bundle.min.js.map:
    * jquery.validate.min.js:
    * bootstrap-grid.min.css.map:
    * bootstrap-reboot.min.css.map:
    * additional-methods.min.js:
    * jquery.validate.unobtrusive.js:
    * jquery.validate.unobtrusive.min.js:

---
+++ TriviaApp/Controllers/QuestionController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TriviaApp.Controllers
+{
+    public class QuestionController : Controller
+    {
+   
+        public string Index()
+        {
+            return HtmlEncoder.Default.Encode("What year did the Wright brothers first take flight?");
+        }
+
+        public string Answer() => HtmlEncoder.Default.Encode("1903");
+    }
+}
