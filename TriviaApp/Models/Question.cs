8f69efd9fae3182962abe76b5e31ea206dddca5b
Barrett Helzel <barrett.p.helzel@gmail.com>
2020-04-23 15:01:00 -0700

    * Question.cs: Create Question model
    * TriviaApp.csproj: Add Question Model
    
    * Index.cshtml: Update Questoin View
    
    * _Layout.cshtml: update navbar links

---
+++ TriviaApp/Models/Question.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace TriviaApp.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string Question { get; set; }
+
+        public Question()
+        {
+            
+        }
+    }
+}
